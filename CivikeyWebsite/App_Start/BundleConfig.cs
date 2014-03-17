fb94ee1659360d8e16326ccf9f3767a0dd41cd71
Idriss =) <ihippocrate@gmail.com>
2014-03-17 10:29:25 +0100

    commit the website V3

---
+++ CivikeyWebsite/App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Optimization;
+using BundleTransformer.Core.Bundles;
+
+namespace CivikeyWebsite
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles( BundleCollection bundles )
+        {
+            bundles.Add( new CustomScriptBundle( "~/bundles/jquery" ).Include(
+                         "~/Content/scripts/jquery-1.10.2.js" ) );
+
+            bundles.Add( new CustomScriptBundle( "~/bundles/scripts/bootstrap" ).Include(
+                      "~/Content/scripts/bootstrap.js" ) );
+
+            bundles.Add( new CustomScriptBundle( "~/bundles/scripts/prefix" ).Include(
+                      "~/Content/scripts/prefixfree.min.js" ) );
+
+            bundles.Add( new CustomStyleBundle( "~/bundles/styles/bootstrap" ).Include(
+                      "~/Content/styles/bootstrap/bootstrap.less" ) );
+
+
+            bundles.Add( new CustomStyleBundle( "~/bundles/styles/site" ).Include(
+                     "~/Content/styles/hover-effects.css", "~/Content/styles/site.less" ) );
+        }
+    }
+}
---
+++ CivikeyWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CivikeyWebsite.MvcApplication" Language="C#" %>
