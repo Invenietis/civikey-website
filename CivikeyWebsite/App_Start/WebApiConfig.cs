ecb3a36687d275eeb7e2d2a48cad9cbe8aa4fc15
Idriss =) <ihippocrate@gmail.com>
2014-04-03 12:07:47 +0200

    remove owin & co

--- CivikeyWebsite/App_Start/WebApiConfig.cs
+++ CivikeyWebsite/App_Start/WebApiConfig.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
-using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 
 namespace CivikeyWebsite
@@ -12,11 +11,6 @@ namespace CivikeyWebsite
     {
         public static void Register( HttpConfiguration config )
         {
-            // Web API configuration and services
-            // Configure Web API to use only bearer token authentication.
-            config.SuppressDefaultHostAuthentication();
-            config.Filters.Add( new HostAuthenticationFilter( OAuthDefaults.AuthenticationType ) );
-
             // Use camel case for JSON data.
             config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
