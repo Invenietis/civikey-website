a5b23b513582c122ef76899ec6450d32bfe0dd32
Cédric Legendre <albundy1385@gmail.com>
2014-07-22 16:02:38 +0200

    Removes Web API adherence

--- CivikeyWebsite/Global.asax.cs
+++ CivikeyWebsite/Global.asax.cs
@@ -1,6 +1,5 @@
 ﻿using System.Configuration;
 using System.Web;
-using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -15,7 +14,6 @@ namespace CivikeyWebsite
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure( WebApiConfig.Register );
             FilterConfig.RegisterGlobalFilters( GlobalFilters.Filters );
             RouteConfig.RegisterRoutes( RouteTable.Routes );
             BundleConfig.RegisterBundles( BundleTable.Bundles );
